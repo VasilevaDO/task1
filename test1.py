9464337c39aa7063d96e3026be75865c95f16dac
VasilevaDO <dashavasileva8@gmail.com>
2015-04-09 19:49:11 +0300

    stack

--- test1.py
+++ test1.py
@@ -1,95 +1,111 @@
 class Stack:
-    def __init__(self,stack_size):
+    
+    def __init__(self):
         
-        self.items = [None]*stack_size
+        self.items = [None]
         self.first = 0
         self.latest = 0
-        
-        
+        self.stack_size = 1
+        self.number_of_elements = 0
+        self.remove_constant = 4  # power of two
+    
+    def __str__(self):
+        return str(self.items)
+    
     def isEmpty(self):
         try:
-                #print('           ',self.first)
-                return self.items[self.first] == None
+            # print('           ',self.first)
+            return self.items[self.first] == None
         except IndexError:
+            self.first = 0
+            # print('           ',self.first)
+            return self.items[self.first] == None
+
+    def add_element(self, item):
+        self.items[self.latest] = item
+        self.number_of_elements += 1
+        self.latest += 1
+    
+    def remove_element(self):
+        x = self.items[self.first]
+        self.items[self.first] = None
+        self.first += 1
+        self.number_of_elements -= 1
+        if self.number_of_elements * \
+                self.remove_constant <= self.stack_size and self.stack_size > 1:
+            if self.first >= self.latest:
+                self.items = self.items[self.first:] + self.items[:self.latest]
+            else:
+                self.items = self.items[self.first:self.latest]
+                self.stack_size //= self.remove_constant
+                self.latest = self.number_of_elements
+            
                 self.first = 0
-                #print('           ',self.first)
-                return self.items[self.first] == None
-        
+            # print(self.stack_size)
+        # print(self.stack_size,self.first,self.latest)
+        return x
+
     def push(self, item):
         try:
-                if self.items[self.latest] == None:
-                        self.items[self.latest] = item
-                        self.latest+=1
-                else:
-                        print('Stack overflow!')
+        # print(self.items)
+        # print(item)
+            if self.items[self.latest] == None:
+                return self.add_element(item)
+            else:
+                return self.create_new_items(item)
+                #print('Stack overflow!')
         except IndexError:
-                self.latest=0
-                if self.items[self.latest] == None:
-                        self.items[self.latest] = item
-                        self.latest+=1
-                else:
-                        print('Stack overflow!')
+            self.latest = 0
+            if self.items[self.latest] == None:
+                return self.add_element(item)
+            else:
+                #print('Stack overflow!')
+                    return self.create_new_items(item)
 
     def pop(self):
         try:
-                if self.items[self.first] != None:
-            
-                        x=self.items[self.first]
-                        self.items[self.first] = None
-                        self.first+=1
-                        
-                        return x
-                else:
-                        print('Stack is empty!')
+            # print(self.items)
+            if self.items[self.first] != None:
+                self.remove_element()
+            else:
+                print(self.peek())
         except IndexError:
-                self.first=0
-                if self.items[self.first] != None:
-                            
-                        x=self.items[self.first]
-                        self.items[self.first] = None
-                        self.first+=1
-                                
-                        return x
-                else:
-                        print('Stack is empty!')
-
-
+            self.first = 0
+            if self.items[self.first] != None:
+                self.remove_element()
+            else:
+                print(self.peek())
 
     def peek(self):
-            if self.items[self.first] != None:
-                    return self.items[self.first]
-            else:
-                    print('Stack is empty!')
+        if self.items[self.first] != None:
+            return self.items[self.first]
+        else:
+            return 'Stack is empty!'
+                
+    def create_new_items(self, item):
+
+        self.latest = self.stack_size
+    
+        self.items = self.items[
+            self.first:] + self.items[:self.first] + [None] * self.stack_size
+        self.stack_size *= 2
+        self.first = 0
         
-    def Print(self):
-            return self.items
+        return self.push(item)
 
 
+s = Stack()
+for i in range(12):
+    s.push(i)
+    print(s)
+for i in range(13):
+    s.pop()
+    print(s)
+for i in range(50, 55):
+    s.push(i)
+    print(s)
 
-
-s = Stack(3)	
-s.push('hello')
-
-s.push('true')
-
-print(s.pop())
-
-#print(s.peek())
-print(s.Print())
-s.push(7)
-s.push(7)
-s.push(7)
-s.push(7)
-s.push(7)
-s.push(7)
-print(s.Print())
-s.push(3)
-print(s.pop())
-s.push(1212)
-s.pop()
-s.pop()
-s.pop()
-s.pop()
-s.push(1212232)
-while not s.isEmpty():
-    print(s.pop())
+# s.push(156)
+# print(s)
+'''while not s.isEmpty():
+    print(s.pop())'''
